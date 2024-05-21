76643829706cb0f5f814b033190af33b99a3924f
abdelwhab Eladawy <abdelwahabeladawy@gmail.com>
2024-05-21 17:20:48 +0300

    add more 2 files and one folder

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// 1 - Write a program that allow to user enter number then print it
+
+// the answer
+
+// var x=prompt("write a number")
+// console.log(x)
+// -----------------------------------------------------------------------------------------------------------
+
+
+
+// 2- Write a program that take number from user then print yes if that number can divide by 3
+// and 4 otherwise print no
+
+// the answer
+// var userNumber = window.prompt("enter anumber ");
+// if (userNumber % 3 == 0 && userNumber % 4 == 0) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+// ------------------------------------------------------------------------------------------------------------
